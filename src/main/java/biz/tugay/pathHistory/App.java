1914e590079636d21279544ef6e9dfaecaa1e9db
koraytugay <koray@tugay.biz>
2019-09-02 12:52:01 -0400

    Initial Commit - First Working Version

---
+++ src/main/java/biz/tugay/pathHistory/App.java
@@ -0,0 +1,8 @@
+package biz.tugay.pathHistory;
+
+
+public class App {
+    public static void main(String[] args) {
+        new PathHistoryJob().startJob();
+    }
+}
---
+++ src/main/java/biz/tugay/pathHistory/PathHistoryJob.java
@@ -0,0 +1,36 @@
+package biz.tugay.pathHistory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static java.nio.file.Files.walkFileTree;
+import static java.nio.file.Paths.get;
+import static java.time.LocalDateTime.now;
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+public class PathHistoryJob {
+    public void startJob() {
+        ScheduledExecutorService es = Executors.newScheduledThreadPool(1);
+        es.scheduleAtFixedRate(this::executeJob, 0, 2, TimeUnit.MINUTES);
+    }
+
+    private void executeJob() {
+        try (FileInputStream is = new FileInputStream("path-history.properties")) {
+            Properties properties = new Properties();
+            properties.load(is);
+            String[] sourceDirs = properties.getProperty("source-paths").split(",");
+            String targetDir = properties.getProperty("target-path");
+            for (String sourceDir : sourceDirs) {
+                PathHistoryWalker pathHistoryWalker = new PathHistoryWalker(get(sourceDir), get(targetDir), ofPattern("YYMMdd_HHmmss").format(now()));
+                walkFileTree(get(sourceDir), pathHistoryWalker);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
